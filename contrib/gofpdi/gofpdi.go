3ab877cb59d63166151b1ee40a7643ee346060eb
Jonathan Law <lawjonathan17@gmail.com>
2020-06-10 18:00:41 -0700

    Error handling

--- contrib/gofpdi/gofpdi.go
+++ contrib/gofpdi/gofpdi.go
@@ -9,6 +9,7 @@ however that use of the default Importer is not thread safe.
 package gofpdi
 
 import (
+	"fmt"
 	"io"
 
 	realgofpdi "github.com/lawjonathan17/gofpdi"
@@ -39,7 +40,7 @@ func NewImporter() *Importer {
 // ImportPage imports a page of a PDF file with the specified box (/MediaBox,
 // /TrimBox, /ArtBox, /CropBox, or /BleedBox). Returns a template id that can
 // be used with UseImportedTemplate to draw the template onto the page.
-func (i *Importer) ImportPage(f gofpdiPdf, sourceFile string, pageno int, box string) int {
+func (i *Importer) ImportPage(f gofpdiPdf, sourceFile string, pageno int, box string) (int, error) {
 	// Set source file for fpdi
 	i.fpdi.SetSourceFile(sourceFile)
 	// return template id
@@ -50,17 +51,19 @@ func (i *Importer) ImportPage(f gofpdiPdf, sourceFile string, pageno int, box st
 // (/MediaBox, TrimBox, /ArtBox, /CropBox, or /BleedBox). Returns a template id
 // that can be used with UseImportedTemplate to draw the template onto the
 // page.
-func (i *Importer) ImportPageFromStream(f gofpdiPdf, rs *io.ReadSeeker, pageno int, box string) int {
+func (i *Importer) ImportPageFromStream(f gofpdiPdf, rs *io.ReadSeeker, pageno int, box string) (int, error) {
 	// Set source stream for fpdi
 	i.fpdi.SetSourceStream(rs)
 	// return template id
 	return i.getTemplateID(f, pageno, box)
 }
 
-func (i *Importer) getTemplateID(f gofpdiPdf, pageno int, box string) int {
+func (i *Importer) getTemplateID(f gofpdiPdf, pageno int, box string) (int, error) {
 	// Import page
-	tpl := i.fpdi.ImportPage(pageno, box)
-
+	tpl, err := i.fpdi.ImportPage(pageno, box)
+	if err != nil {
+		return 0, fmt.Errorf("getTemplateID - import page: %s", err)
+	}
 	// Import objects into current pdf document
 	// Unordered means that the objects will be returned with a sha1 hash instead of an integer
 	// The objects themselves may have references to other hashes which will be replaced in ImportObjects()
@@ -83,7 +86,7 @@ func (i *Importer) getTemplateID(f gofpdiPdf, pageno int, box string) int {
 	// Import gofpdi object hashes and their positions into gopdf
 	f.ImportObjPos(importedObjPos)
 
-	return tpl
+	return tpl, nil
 }
 
 // UseImportedTemplate draws the template onto the page at x,y. If w is 0, the
@@ -112,7 +115,7 @@ var fpdi = NewImporter()
 // /TrimBox, /ArtBox, /CropBox, or /BleedBox). Returns a template id that can
 // be used with UseImportedTemplate to draw the template onto the page.
 // Note: This uses the default Importer. Call NewImporter() to obtain a custom Importer.
-func ImportPage(f gofpdiPdf, sourceFile string, pageno int, box string) int {
+func ImportPage(f gofpdiPdf, sourceFile string, pageno int, box string) (int, error) {
 	return fpdi.ImportPage(f, sourceFile, pageno, box)
 }
 
@@ -121,7 +124,7 @@ func ImportPage(f gofpdiPdf, sourceFile string, pageno int, box string) int {
 // that can be used with UseImportedTemplate to draw the template onto the
 // page.
 // Note: This uses the default Importer. Call NewImporter() to obtain a custom Importer.
-func ImportPageFromStream(f gofpdiPdf, rs *io.ReadSeeker, pageno int, box string) int {
+func ImportPageFromStream(f gofpdiPdf, rs *io.ReadSeeker, pageno int, box string) (int, error) {
 	return fpdi.ImportPageFromStream(f, rs, pageno, box)
 }
 
